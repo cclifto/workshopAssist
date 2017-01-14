7ec47d4ba38099f3a9f687bee2328fa1c505c348
cclifo <cliftonconner@gmail.com>
2017-01-14 13:46:55 -0600

    can post new bulletins to API

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
+// CODE TO FETCH BULLETINS
+
 var bulletinBoardEl = document.querySelector('#bulletin-board')
 
 function handleResponse(bulletinsArray){
+
+	bulletinsArray = bulletinsArray.reverse()
+
 	var html = '<h2>bulletins loaded!</h2>'
 	//FOR every bulletin, we will write an HTML element and insert the content.
 	
 
 	for (var position = 0; position < bulletinsArray.length; position += 1){
 		var bulletin = bulletinsArray[position]
-		html += '<div>'
+		html += '<div class= "bulletin">'
 		html +=		'<h3>' + bulletin.title + '</h3>'
 		html += 	'<p>' + bulletin.content + '</p>'
 		html += 	'<p>' + bulletin.signed + '</p>'
@@ -17,8 +22,29 @@ function handleResponse(bulletinsArray){
 
 }
 
-$.get('https://tiy-bulletin-board.herokuapp.com/api/bulletin')
-	.then(handleResponse)
+var fetchAndRenderBulletins = function(){
+	$.get('https://tiy-bulletin-board.herokuapp.com/api/bulletin')
+		.then(handleResponse)
 
-bulletinBoardEl.innerHTML = '<img src="Loading_icon.gif">'
+	bulletinBoardEl.innerHTML = '<img src="Loading_icon.gif">'
+}
 
+//CODE TO POST A BULLETIN
+
+var formEl = document.querySelector('form')
+
+var handleFormSubmit = function(eventObj){
+	eventObj.preventDefault()
+	var bulletinObject = {
+			title: eventObj.target.title.value,
+			content: eventObj.target.content.value,
+			signed: eventObj.target.signed.value
+	}
+	$.post('https://tiy-bulletin-board.herokuapp.com/api/bulletin', bulletinObject)
+		.then(fetchAndRenderBulletins)
+		eventObj.target.reset()
+}
+
+formEl.onsubmit = handleFormSubmit // sets up handleFormSubmit to run when the "submit" button is clicked
+
+fetchAndRenderBulletins()
