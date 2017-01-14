eac4398b61ce59d0b955a7eda89ed3fc222c12aa
cclifo <cliftonconner@gmail.com>
2017-01-14 13:01:33 -0600

    pulling and posting all 20 posts

--- js/script.js
+++ js/script.js
@@ -3,14 +3,18 @@ var bulletinBoardEl = document.querySelector('#bulletin-board')
 function handleResponse(bulletinsArray){
 	var html = '<h2>bulletins loaded!</h2>'
 	//FOR every bulletin, we will write an HTML element and insert the content.
-	var firstBulletin = bulletinsArray[0]
-	console.log(firstBulletin)
-	html += '<div>'
-	html +=		'<h3>' + firstBulletin.title + '</h3>'
-	html += 	'<p>' + firstBulletin.content + '</p>'
-	html += 	'<p>' + firstBulletin.signed + '</p>'
-	html += '</div>'
+	
+
+	for (var position = 0; position < bulletinsArray.length; position += 1){
+		var bulletin = bulletinsArray[position]
+		html += '<div>'
+		html +=		'<h3>' + bulletin.title + '</h3>'
+		html += 	'<p>' + bulletin.content + '</p>'
+		html += 	'<p>' + bulletin.signed + '</p>'
+		html += '</div>'
+	}
 	bulletinBoardEl.innerHTML = html
+
 }
 
 $.get('https://tiy-bulletin-board.herokuapp.com/api/bulletin')
