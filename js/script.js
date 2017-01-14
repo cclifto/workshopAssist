1afb2be4ea0453d9855622f12a58204cf01255e2
cclifo <cliftonconner@gmail.com>
2017-01-14 12:03:04 -0600

    added loading gif

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+var bulletinBoardEl = document.querySelector('#bulletin-board')
+0
 console.log('here be javascript')
 $.get('https://tiy-bulletin-board.herokuapp.com/api/bulletin')
 	.then(handleResponse)
 
-console.log('i just requested the bulletin')
+bulletinBoardEl.innerHTML = '<img src="Loading_icon.gif">'
 
 function handleResponse(serverResponse){
 	console.log(serverResponse)
+	bulletinBoardEl.innerHTML = '<h2>bulletins loaded!</h2>'
 }
