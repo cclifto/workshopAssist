8c79fc57b45be3fe0bdd19443a8bd0e6d49a874d
cclifo <cliftonconner@gmail.com>
2017-01-14 12:55:20 -0600

    posted a bulletin

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 var bulletinBoardEl = document.querySelector('#bulletin-board')
 
-console.log('here be javascript')
+function handleResponse(bulletinsArray){
+	var html = '<h2>bulletins loaded!</h2>'
+	//FOR every bulletin, we will write an HTML element and insert the content.
+	var firstBulletin = bulletinsArray[0]
+	console.log(firstBulletin)
+	html += '<div>'
+	html +=		'<h3>' + firstBulletin.title + '</h3>'
+	html += 	'<p>' + firstBulletin.content + '</p>'
+	html += 	'<p>' + firstBulletin.signed + '</p>'
+	html += '</div>'
+	bulletinBoardEl.innerHTML = html
+}
+
 $.get('https://tiy-bulletin-board.herokuapp.com/api/bulletin')
 	.then(handleResponse)
 
 bulletinBoardEl.innerHTML = '<img src="Loading_icon.gif">'
 
-function handleResponse(serverResponse){
-	console.log(serverResponse)
-	bulletinBoardEl.innerHTML = '<h2>bulletins loaded!</h2>'
-}
